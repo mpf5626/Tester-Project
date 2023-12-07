8f5b4d9877384c92bd3178ec0c215c971f928787
mpf5626 <150702624+mpf5626@users.noreply.github.com>
2023-12-07 02:24:59 +0000

    finished

--- app.py
+++ app.py
@@ -7,21 +7,33 @@ app = Flask(__name__)
 def home():
     return render_template('index.html')
 
+# ...
+
 @app.route('/weather', methods=['POST'])
 def weather():
     city = request.form['city']
-    api_key = 'e708f9298fb72d34d100684f34dd369e'
-    api_url = f'http://api.openweathermap.org/data/2.5/weather?q={city}&appid={api_key}'
+    api_key = 'e708f9298fb72d34d100684f34dd369e'  # Replace this with your actual API key
+    api_url = f'http://api.openweathermap.org/data/2.5/weather?q={city}&appid={api_key}&units=metric'
+    
     response = requests.get(api_url)
     data = response.json()
 
-    weather_data = {
-        'city': data['name'],
-        'temperature': data['main']['temp'],
-        'description': data['weather'][0]['description'],
-    }
+    if 'name' in data:
+        weather_data = {
+            'city': data['name'],
+            'temperature': data['main']['temp'],
+            'description': data['weather'][0]['description'],
+            'humidity': data['main']['humidity'],
+            'wind_speed': data['wind']['speed'],
+            'icon': data['weather'][0]['icon'],
+        }
+        return render_template('weather.html', weather_data=weather_data)
+    else:
+        error_message = 'City not found. Please enter a valid city name.'
+        return render_template('error.html', error_message=error_message)
 
-    return render_template('weather.html', weather_data=weather_data)
+# ...
+
 
 if __name__ == '__main__':
     app.run(debug=True)
