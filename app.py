4991f59835f5770b5103496e71482c5574db24f9
nicktaweel <149599293+nicktaweel@users.noreply.github.com>
2023-12-10 22:10:20 +0000

    Added precipitation, farenheit temperature, date, and time

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template, request
 import requests
+from datetime import datetime, timedelta
 
 app = Flask(__name__)
 
@@ -19,14 +20,36 @@ def weather():
     data = response.json()
 
     if 'name' in data:
+
+        # Extract timezone information from OpenWeatherMap API
+        timezone_offset = data['timezone']
+
+        # Calculate current time using timezone information
+        current_utc_time = datetime.utcnow()
+        current_local_time = current_utc_time + timedelta(seconds=timezone_offset)
+
+        # Extract Celsius temperature
+        temperature_celsius = round(data['main']['temp'], 1)
+
+        # Extract Fahrenheit temperature
+        temperature_fahrenheit = round(temperature_celsius * 9/5 + 32, 1)
+        
+        # Extract precipitation percentage
+        precipitation_percent = data.get('rain', {}).get('1h', 0)
+
         weather_data = {
             'city': data['name'],
-            'temperature': data['main']['temp'],
+            'temperature_celsius': temperature_celsius,
+            'temperature_fahrenheit': temperature_fahrenheit,
             'description': data['weather'][0]['description'],
             'humidity': data['main']['humidity'],
             'wind_speed': data['wind']['speed'],
             'icon': data['weather'][0]['icon'],
+            'timezone': data['timezone'],
+            'current_time': current_local_time.strftime('%Y-%m-%d %H:%M:%S'),
+            'precipitation_percent': precipitation_percent,
         }
+        
         return render_template('weather.html', weather_data=weather_data)
     else:
         error_message = 'City not found. Please enter a valid city name.'
